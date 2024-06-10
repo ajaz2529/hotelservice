b3b40aca4fb9d288aeec29c94dc0d069cbab2b18
AJAZ BHAT <ajazbhat2529@gmail.com>
2024-06-10 23:14:32 +0530

    Ajaz Bhat

--- src/test/java/com/hotelservice/HotelServiceApplicationTests.java
+++ src/test/java/com/hotelservice/HotelServiceApplicationTests.java
@@ -1,13 +1,40 @@
 package com.hotelservice;
 
+import com.hotelservice.entity.Hotel;
+import com.hotelservice.exception.ResourceNotFoundException;
+import com.hotelservice.repository.HotelRepository;
+import com.hotelservice.service.HotelService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Optional;
 
 @SpringBootTest
 class HotelServiceApplicationTests {
+	@Autowired
+	private HotelRepository hotelRepository;
+	@Autowired
+	private HotelService hotelService;
 
 	@Test
 	void contextLoads() {
+	}
+	@Test
+	void print(){
+		System.out.println("test");
+	}
+
+	@Test
+	public void getHotel() {
+		try{
+			Optional<Hotel> hotel1 = hotelRepository.findById("hotel1");
+			Hotel hotel = hotel1.get();
+			System.out.println(hotel);
+		}catch (Exception e){
+			e.printStackTrace();
+
+		}
 	}
 
 }
